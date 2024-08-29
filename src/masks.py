0d2e0024d6542cdbffc00250b0c4a1f0ccf3d9f2
aleksandra <a.surkova90@mail.ru>
2024-08-29 18:11:08 +0300

    fixed

--- src/masks.py
+++ src/masks.py
@@ -6,12 +6,7 @@ def get_mask_card_number(user_card_number: Union[int, str]) -> Union[int, str]:
 
     user_card_number = str(user_card_number)
 
-    return "".join(user_card_number[-16:-12] + " " + user_card_number[-12:-10] + "** **** " + user_card_number[-4:])
-
-
-result = get_mask_card_number(7000792289606361)
-
-print(result)
+    return f"{user_card_number[-16:-12]} {user_card_number[-12:-10]}** **** {user_card_number[-4:]}"
 
 
 def get_mask_account(user_account_number: Union[int, str]) -> Union[int, str]:
@@ -19,9 +14,4 @@ def get_mask_account(user_account_number: Union[int, str]) -> Union[int, str]:
 
     user_account_number = str(user_account_number)
 
-    return "".join("**" + user_account_number[-4:])
-
-
-result = get_mask_account(73654108430135874305)
-
-print(result)
+    return f"**{user_account_number[-4:]}"
--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,8 @@
 import re
 from src.masks import get_mask_card_number, get_mask_account
 
-def mask_account_card(number_card_or_account):
+
+def mask_account_card(number_card_or_account: str) -> str | None:
     """Обрабатывает информацию о картах и счетах"""
 
     if "Счет" in number_card_or_account:
@@ -9,8 +10,8 @@ def mask_account_card(number_card_or_account):
     else:
         return number_card_or_account[0:-16] + get_mask_card_number(number_card_or_account)
 
-result = mask_account_card("Счет 35383033474447895560")
-result_2 = mask_account_card("Visa Gold 5999414228426353")
 
-print(result)
-print(result_2)
+def get_date(date: str) -> str | None:
+    """возвращает строку с датой в формате "ДД.ММ.ГГГГ ("11.03.2024")"""
+
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
