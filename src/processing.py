1cc91fbb4dd7281aa9e6936ba9f2a6564e6a85cd
aleksandra <a.surkova90@mail.ru>
2024-09-05 14:39:59 +0300

    Добавлена функция которая возвращает новый список отсортированный по дате

--- src/processing.py
+++ src/processing.py
@@ -17,3 +17,23 @@ print(
         "EXECUTED",
     )
 )
+
+
+def sort_by_date(my_lists, sort_date=True):
+    """Функция возвращает новый список, отсортированный по дате"""
+
+    new_list = [sorted(my_lists, key=lambda x: x["date"], reverse=sort_date)]
+
+    return new_list
+
+
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
