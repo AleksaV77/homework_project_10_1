6ae0be19c41454b2f89c5af1b44ff61e4d8798ae
aleksandra <a.surkova90@mail.ru>
2024-09-09 17:10:51 +0300

    Добавлен файл с фикстурами

--- src/masks.py
+++ src/masks.py
@@ -4,9 +4,17 @@ from typing import Union
 def get_mask_card_number(user_card_number: Union[int, str]) -> Union[int, str]:
     """Маскировка номера банковской карты"""
 
-    user_card_number = str(user_card_number)
+    user_card_number_str = str(user_card_number)
 
-    return f"{user_card_number[-16:-12]} {user_card_number[-12:-10]}** **** {user_card_number[-4:]}"
+    if len(user_card_number_str) == 16:
+        return f"{user_card_number_str[-16:-12]} {user_card_number_str[-12:-10]}** **** {user_card_number_str[-4:]}"
+    elif user_card_number_str == "" or user_card_number_str == " ":
+        return "Нет номера карты или счета"
+    elif len(user_card_number_str) < 16 or len(user_card_number_str) > 16:
+        return "Не правильный номер"
+
+
+print(get_mask_card_number("2546"))
 
 
 def get_mask_account(user_account_number: Union[int, str]) -> Union[int, str]:
@@ -14,4 +22,12 @@ def get_mask_account(user_account_number: Union[int, str]) -> Union[int, str]:
 
     user_account_number = str(user_account_number)
 
-    return f"**{user_account_number[-4:]}"
+    if len(user_account_number) == 20:
+        return f"**{user_account_number[-4:]}"
+    elif user_account_number == "" or user_account_number == " ":
+        return "Нет номера карты или счета"
+    elif len(user_account_number) < 20 or len(user_account_number) > 20:
+        return "Не правильный номер"
+
+
+print(get_mask_account(" "))
--- src/widget.py
+++ src/widget.py
@@ -6,13 +6,29 @@ from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(number_card_or_account: str) -> str | None:
     """Обрабатывает информацию о картах и счетах"""
 
-    if "Счет" in number_card_or_account:
-        return "Счет " + get_mask_account(number_card_or_account)
-    else:
-        return number_card_or_account[0:-16] + get_mask_card_number(number_card_or_account)
+    card_or_account_info = number_card_or_account.split(" ")
+
+    if "Счет" in card_or_account_info:
+        if len(card_or_account_info[-1]) == 20:
+            return f"{" ".join(card_or_account_info[:-1])} {get_mask_account(card_or_account_info[-1])}"
+        else:
+            return f"{get_mask_account(card_or_account_info[-1])}"
+    elif "Счет" not in card_or_account_info:
+        if len(card_or_account_info[-1]) == 16:
+            return f"{" ".join(card_or_account_info[:-1])} {get_mask_card_number(card_or_account_info[-1])}"
+        else:
+            return f"{get_mask_card_number(card_or_account_info[-1])}"
+
+
+print(mask_account_card("567"))
 
 
 def get_date(date: str) -> str | None:
     """возвращает строку с датой в формате "ДД.ММ.ГГГГ ("11.03.2024")"""
 
-    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+    if date == "":
+        return "Отсутствует дата"
+    else:
+        return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+
+print(get_date("2024-03-11T02:26:18.671407"))
