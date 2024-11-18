cec692e19370422f871f268c02d96b1121aab44a
aleksandra <a.surkova90@mail.ru>
2024-11-18 16:09:39 +0300

    homework_12_2

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,15 @@
 import json
+import logging
 from json import JSONDecodeError
+
+logger = logging.getLogger()
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s: %(filename)s: %(levelname)s: %(message)s",
+    filename="..//logs//utils.log",
+    encoding="utf-8",
+    filemode="w",
+)
 
 
 def list_financial_transactions(file):
@@ -8,8 +18,10 @@ def list_financial_transactions(file):
     with open(file, "r", encoding="utf-8") as f:
         try:
             operations = json.load(f)
+            logging.info("Список словарей с данными о финансовых транзакциях")
             return operations
         except JSONDecodeError:
+            logging.error("Пустой список")
             return []
 
 
